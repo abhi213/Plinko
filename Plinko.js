6cf23fabe0a5c7994be5d03f08201bf6c4e80ea1
abhi213 <akaletlasub@gmail.com>
2020-12-05 12:05:24 +1300

    Add files via upload

--- Plinko.js
+++ Plinko.js
@@ -1,21 +1,29 @@
-class Plinko{
-    constructor(x, y){
+class Plinko {
+    constructor(x, y) {
         var options = {
-           isStatic : true
+            restitution: 1,
+            friction: 0,
+            isStatic:true
         }
         this.r = 10;
-        this.body = Bodies.circle(x, y, this.r, options);  
-        this.body.collisionFilter.group = 1;
+        this.body = Bodies.circle(x, y, this.r, options);
+        
         World.add(world, this.body);
     }
-    
-    display(){
+    display() {
+
         var pos = this.body.position;
+        var angle = this.body.angle;
+
         push();
-        translate(0, 0);
-        ellipseMode(RADIUS);
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
         fill("white");
-        ellipse(pos.x, pos.y, this.r/2, this.r/2);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
         pop();
     }
-}
+
+};
